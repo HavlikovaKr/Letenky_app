116f8081b927e640ac111d670887035e84aafc39
Kristýna Havlíková <kr.havlikova17@gmail.com>
2024-10-17 14:11:53 +0200

    Vypsaní nabídky

---
+++ Letenky_app.py
@@ -0,0 +1,26 @@
+# Aplikace na letenky 
+
+city = { 
+
+    "Londýn": 1000, 
+    "Berlín": 500, 
+    "Řím": 1200, 
+    "Madrid": 1500, 
+    "Budapešť:": 900 
+
+} 
+
+line = 35 * "=" 
+offers = """ 
+Londýn     |   1000,-Kč 
+Berlín     |   500,-Kč 
+Řím        |   1200,-Kč 
+Madrid     |   1500,-Kč 
+Budapešť   |   900,-Kč 
+""" 
+
+ 
+
+print("VÍTEJTE V APLIKACI LETENKY! 'ODLET PRAHA'") 
+print (line, offers, end='') 
+print(line) 
