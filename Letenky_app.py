458b180fa0a21ce90d2c8af5ad5204f279bd8711
Kristýna Havlíková <kr.havlikova17@gmail.com>
2024-10-17 14:49:15 +0200

    Přidání proměnných destinace, people, children, luggage

--- Letenky_app.py
+++ Letenky_app.py
@@ -19,8 +19,13 @@ Madrid     |   1500,-Kč
 Budapešť   |   900,-Kč 
 """ 
 
- 
-
-print("VÍTEJTE V APLIKACI LETENKY! 'ODLET PRAHA'") 
-print (line, offers, end='') 
-print(line) 
+# print("VÍTEJTE V APLIKACI EVROPA LETENKY! 'ODLET PRAHA'") 
+# print (line, offers, end='') 
+# print(line) 
+print(" 'OSOBNÍ ÚDAJE' ")
+destinace = input("VYBERTE CÍLOVÉ MĚSTO: "). lower()
+people = int(input("POČET DOSPĚLÝCH: "))
+children = int(input("POČET DĚTÍ DO 10 LET: "))
+luggage = int(input("POČET ZAVAZADEL DO 15KG: "))
+# NAD 15 KG PŘÍPLATEK 200KČ
+# print(f"Vaše cílové město: {destinace}\nPočet dospělých osob: {people}\nPočet dětí: {children}\nPočet zavazadel do 15KG: {luggage}")
