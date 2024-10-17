ff443682098475bd767b8dc22a861524393255c6
Kristýna Havlíková <kr.havlikova17@gmail.com>
2024-10-17 16:44:48 +0200

    Upravenné proměnné - inputy

--- Letenky_app.py
+++ Letenky_app.py
@@ -1,31 +1,38 @@
 # Aplikace na letenky 
+city = { "Londýn": 1000, "Berlín": 500, "Řím": 1200, "Madrid": 1500, "Budapešť": 900 } 
 
-city = { 
-
-    "Londýn": 1000, 
-    "Berlín": 500, 
-    "Řím": 1200, 
-    "Madrid": 1500, 
-    "Budapešť:": 900 
-
-} 
-
-line = 35 * "=" 
+line = 35 * "_" 
+double_line = 35 * "="
 offers = """ 
-Londýn     |   1000,-Kč 
-Berlín     |   500,-Kč 
-Řím        |   1200,-Kč 
-Madrid     |   1500,-Kč 
-Budapešť   |   900,-Kč 
-""" 
+(VÍTEJTE V APLIKACI EVROPA LETENKY!
+***ODLET POUZE Z PRAHY***
+-----------------------------------
+          "DESTINACE"
+1 - Londýn    |   1000,-Kč 
+2 - Berlín    |   500,-Kč 
+3 - Řím       |   1200,-Kč 
+4 - Madrid    |   1500,-Kč 
+5 - Budapešť  |   900,-Kč 
+-----------------------------------
+"SLEVA NA LETENKY"
+DĚTI DO 10 LET - 50%
+-----------------------------------
+"CENA ZAVAZADEL"
+DO 15KG CENA: 100,-Kč
+NAD 15KG CENA: 200,-KČ
+***MOŽNOST POUZE JEDNO ZAVAZADLO NA OSOBU NEBO DÍTĚ***  
+"""
+# print (double_line, offers, end='') 
+# print(double_line) 
 
-# print("VÍTEJTE V APLIKACI EVROPA LETENKY! 'ODLET PRAHA'") 
-# print (line, offers, end='') 
-# print(line) 
-print(" 'OSOBNÍ ÚDAJE' ")
-destinace = input("VYBERTE CÍLOVÉ MĚSTO: "). lower()
-people = int(input("POČET DOSPĚLÝCH: "))
+print("'INFORMACE'")
+
+destinace_num = int(input("VYBERTE ČÍSLO DESTINACE: "))
+destinace = list(city.keys())[destinace_num - 1]  # Výběr destinace podle čísla
+
 children = int(input("POČET DĚTÍ DO 10 LET: "))
-luggage = int(input("POČET ZAVAZADEL DO 15KG: "))
-# NAD 15 KG PŘÍPLATEK 200KČ
-# print(f"Vaše cílové město: {destinace}\nPočet dospělých osob: {people}\nPočet dětí: {children}\nPočet zavazadel do 15KG: {luggage}")
+adult = int(input("POČET OSOB: "))
+
+
+weights_input = input("ZADEJTE VÁHY ZAVAZADEL ODDĚLENO ČÁRKOU (např. 16.5, 17): ")
+
